ab15a424f0c98de78e72ca2ed2617d5034d7b7c0
Jack Gisel <jackrgisel@gmail.com>
2020-04-13 16:27:03 -0700

    Calendar css working

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,34 +6,34 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
 class Schedule extends Component {
-    handleLogout = () => {
-        const { dispatch } = this.props;
-        dispatch(logoutUser());
-      };
+  handleLogout = () => {
+    const { dispatch } = this.props;
+    dispatch(logoutUser());
+  };
 
-    render() {
-        return ( 
-            <div>
-                <FullCalendar
-                        defaultView="dayGridMonth" 
-                        plugins={[dayGridPlugin, interactionPlugin]}
-                        editable={true}
-                        eventDrop={this.handleEventDrop}
-                        eventClick={this.handleEventClick}
-                        events={[
-                            { title: 'event 1', date: '2019-04-01' },
-                            { title: 'event 2', date: '2019-04-02' }
-                        ]}
-                />
-                <button onClick={this.handleLogout}>Logout</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <FullCalendar
+          defaultView='dayGridMonth'
+          plugins={[dayGridPlugin, interactionPlugin]}
+          editable={true}
+          eventDrop={this.handleEventDrop}
+          eventClick={this.handleEventClick}
+          events={[
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+          ]}
+        />
+        <button onClick={this.handleLogout}>Logout</button>
+      </div>
+    );
+  }
 }
 function mapStateToProps(state) {
-    return {
-      isLoggingOut: state.auth.isLoggingOut,
-      logoutError: state.auth.logoutError
-    };
-  }
+  return {
+    isLoggingOut: state.auth.isLoggingOut,
+    logoutError: state.auth.logoutError,
+  };
+}
 export default connect(mapStateToProps)(Schedule);
