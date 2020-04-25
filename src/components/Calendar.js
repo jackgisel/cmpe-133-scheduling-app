404e94da8796da83dd47a08f6811827c6499b593
Jack Gisel <jackrgisel@gmail.com>
2020-04-25 11:29:22 -0700

    quick fix

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import { Container, Paper, makeStyles } from '@material-ui/core';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { Container, Paper, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -14,25 +14,24 @@ const useStyles = makeStyles((theme) => ({
 export default (props) => {
   const classes = useStyles();
   return (
-    <Container className={classes.container} component='schedule' maxWidth='md'>
+    <Container className={classes.container} component="schedule" maxWidth="md">
       <Paper>
         <FullCalendar
-          defaultView='timeGridWeek'
+          defaultView="timeGridWeek"
           allDaySlot={false}
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           editable={true}
-          minTime='7'
           // eventDrop={this.handleEventDrop}
           // eventClick={this.handleEventClick}
           events={[
-            { title: 'event 1', date: '2020-04-14' },
-            { title: 'event 2', date: '2020-04-12' },
+            { title: "event 1", date: "2020-04-14" },
+            { title: "event 2", date: "2020-04-12" },
           ]}
           header={[
             {
-              left: 'prev,next today',
-              center: 'title',
-              right: 'month,agendaWeek,agendaDay,list',
+              left: "prev,next today",
+              center: "title",
+              right: "month,agendaWeek,agendaDay,list",
             },
           ]}
         />
