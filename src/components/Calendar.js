f9d9b7a9a59ef1bbd61eacb0f0295fb4041f1d9c
Luis <luis.avila@sjsu.edu>
2020-04-25 11:48:05 -0700

    Added dropdowns without css fix

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,9 +5,23 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Container, Paper, makeStyles } from "@material-ui/core";
 
+
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: 30,
+  },
+  formControl: {
+    margin: 1,
+    minWidth: 120,
+  },
+  addcourse: {
+    marginTop: 20,
   },
 }));
 
@@ -16,6 +30,45 @@ export default (props) => {
   return (
     <Container className={classes.container} component="schedule" maxWidth="md">
       <Paper>
+        <FormControl fullWidth className={classes.formControl}>
+          <InputLabel id='label'>Select Department</InputLabel>
+          <Select labelId='label' id='select' value=''>
+            <MenuItem value='10'>Ten</MenuItem>
+            <MenuItem value='20'>Twenty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <br></br>
+        <FormControl fullWidth className={classes.formControl}>
+          <InputLabel id='label2'>Select Course</InputLabel>
+          <Select labelId='label2' id='select' value=''>
+            <MenuItem value='10'>Ten</MenuItem>
+            <MenuItem value='20'>Twenty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <br></br>
+
+        <FormControl fullWidth className={classes.formControl}>
+          <InputLabel id='label3'>Select Section</InputLabel>
+          <Select labelId='label3' id='select' value=''>
+            <MenuItem value='10'>Ten</MenuItem>
+            <MenuItem value='20'>Twenty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          type='button'
+          fullWidth
+          variant='contained'
+          color='primary'
+          //onClick={this.handleSubmit}
+          className={classes.addcourse}
+        >
+          Add course to schedule
+        </Button>
+
+
         <FullCalendar
           defaultView="timeGridWeek"
           allDaySlot={false}
