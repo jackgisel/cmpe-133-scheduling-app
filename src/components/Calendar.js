4b5b50840a687b2410ee886b4589e164c8586ddb
Jack Gisel <jackrgisel@gmail.com>
2020-04-26 17:43:28 -0700

    create user in datatbase on signup

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,7 +3,14 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { Container, Paper, makeStyles } from "@material-ui/core";
+import {
+  Container,
+  Paper,
+  makeStyles,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getDepartments, getCourses, getSections } from "../actions/";
@@ -57,94 +64,110 @@ export default (props) => {
     }
   }, [selectedCourse]);
 
+  function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  }
+
   return (
     <Container className={classes.container} component="schedule" maxWidth="md">
       <Paper>
-        <div className={classes.padded}>
-          <FormControl
-            fullWidth
-            className={classes.formControl}
-            disabled={departments.length <= 0}
-          >
-            <InputLabel id="label">Select Department</InputLabel>
-            <Select
-              labelId="label"
-              id="select"
-              value={selectedDepartment}
-              onChange={(event) => {
-                setSelectedDepartment(event.target.value);
-                setSelectedCourse(undefined);
-                setSelectedSection(undefined);
-              }}
+        <div>
+          <div className={classes.padded}>
+            <FormControl
+              fullWidth
+              className={classes.formControl}
+              disabled={departments.length <= 0}
             >
-              {departments?.map((department) => (
-                <MenuItem value={department.id}>
-                  {department["Department Name"]}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-
-          <br></br>
-          <FormControl
-            fullWidth
-            className={classes.formControl}
-            disabled={courses.length <= 0}
-          >
-            <InputLabel id="label2">Select Course</InputLabel>
-            <Select
-              labelId="label2"
-              id="select"
-              value={selectedCourse}
-              onChange={(event) => {
-                setSelectedCourse(event.target.value);
-                setSelectedSection(undefined);
-              }}
-            >
-              {courses?.map((course) => (
-                <MenuItem value={course.id}>
-                  {course["Course"]} {course["Title"]}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-
-          <br></br>
-
-          <FormControl
-            fullWidth
-            className={classes.formControl}
-            disabled={sections.length <= 0}
-          >
-            <InputLabel id="label3">Select Section</InputLabel>
-            <Select
-              labelId="label3"
-              id="select"
-              value={selectedSection}
-              onChange={(event) => setSelectedSection(event.target.value)}
-            >
-              {sections?.map((section) => {
-                console.log(section);
-                return (
-                  <MenuItem value={section.id}>
-                    {section["Section"]} {section["Instructor Fname"]}.
-                    {section["Instructor Lname"]}
+              <InputLabel id="label">Select Department</InputLabel>
+              <Select
+                labelId="label"
+                id="select"
+                value={selectedDepartment}
+                onChange={(event) => {
+                  setSelectedDepartment(event.target.value);
+                  setSelectedCourse(undefined);
+                  setSelectedSection(undefined);
+                }}
+              >
+                {departments?.map((department) => (
+                  <MenuItem value={department.id}>
+                    {department["Department Name"]}
                   </MenuItem>
-                );
-              })}
-            </Select>
-          </FormControl>
+                ))}
+              </Select>
+            </FormControl>
 
-          <Button
-            type="button"
-            fullWidth
-            variant="contained"
-            color="primary"
-            //onClick={this.handleSubmit}
-            className={classes.addcourse}
-          >
-            Add course to schedule
-          </Button>
+            <br></br>
+            <FormControl
+              fullWidth
+              className={classes.formControl}
+              disabled={courses.length <= 0}
+            >
+              <InputLabel id="label2">Select Course</InputLabel>
+              <Select
+                labelId="label2"
+                id="select"
+                value={selectedCourse}
+                onChange={(event) => {
+                  setSelectedCourse(event.target.value);
+                  setSelectedSection(undefined);
+                }}
+              >
+                {courses?.map((course) => (
+                  <MenuItem value={course.id}>
+                    {course["Course"]} {course["Title"]}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <br></br>
+
+            <FormControl
+              fullWidth
+              className={classes.formControl}
+              disabled={sections.length <= 0}
+            >
+              <InputLabel id="label3">Select Section</InputLabel>
+              <Select
+                labelId="label3"
+                id="select"
+                value={selectedSection}
+                onChange={(event) => setSelectedSection(event.target.value)}
+              >
+                {sections?.map((section) => {
+                  console.log(section);
+                  return (
+                    <MenuItem value={section.id}>
+                      {section["Section"]} {section["Instructor Fname"]}.
+                      {section["Instructor Lname"]}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              //onClick={this.handleSubmit}
+              className={classes.addcourse}
+            >
+              Add course to schedule
+            </Button>
+          </div>
+          <div>
+            <List component="nav" aria-label="secondary mailbox folders">
+              <ListItem button>
+                <ListItemText primary="Trash" />
+              </ListItem>
+              <ListItemLink href="#simple-list">
+                <ListItemText primary="Spam" />
+              </ListItemLink>
+            </List>
+          </div>
         </div>
 
         <FullCalendar
